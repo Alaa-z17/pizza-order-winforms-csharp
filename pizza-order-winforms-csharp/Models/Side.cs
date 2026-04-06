924a6e82482c9b63acb7c7c7a33418e727073dcc
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-04-06 12:39:10 +0300

    added side.cs

---
+++ pizza-order-winforms-csharp/Models/Side.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaOrderSystem.Models
+{
+    public class Side : BaseItem
+    {
+        public SideType SideType { get; set; }
+        public Side(string name, int quantity, SideType type) : base(name, quantity)
+        {
+            SideType = type;
+            UnitPrice = type == SideType.Fries ? 2.49m : type == SideType.OnionRings ? 3.49m : type == SideType.GarlicBread ? 2.99m : 3.99m;
+        }
+        public override string GetDescription() => $"{SideType}";
+    }
+}

c108b1c8296d1657d43b8521782e9ae70496f525
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-04-06 12:35:02 +0300

    added Drink.cs

---
+++ pizza-order-winforms-csharp/Models/Drink.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaOrderSystem.Models
+{
+    public class Drink : BaseItem
+    {
+        public DrinkSize Size { get; set; }
+        public Drink(string name, int quantity, DrinkSize size) : base(name, quantity)
+        {
+            Size = size;
+            UnitPrice = size == DrinkSize.Small ? 1.99m : size == DrinkSize.Medium ? 2.49m : 2.99m;
+        }
+        public override string GetDescription() => $"{Size} {Name}";
+    }
+}

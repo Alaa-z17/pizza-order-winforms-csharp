b7cf6f675ae5c3cd675797845b93deb99fd45951
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-04-06 12:21:23 +0300

    added BaseItem.cs

---
+++ pizza-order-winforms-csharp/Models/BaseItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PizzaOrderSystem.Models
+{
+    public abstract class BaseItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; protected set; }
+
+        protected BaseItem(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        // Virtual method – polymorphism
+        public virtual decimal CalculateTotal() => UnitPrice * Quantity;
+
+        public abstract string GetDescription(); // forces override
+    }
+}

4415add66d057aa0b6d5f5c5d65dc99f15d1d579
Alaa Alkatshah <alaaalkatshah@gmail.com>
2026-04-06 12:11:32 +0300

    added Enums.cs and appsettings.json

---
+++ pizza-order-winforms-csharp/Models/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaOrderSystem.Models
+{
+    public enum PizzaSize { Small, Medium, Large, Family }
+    public enum CrustType { Thin, Thick, Stuffed, GlutenFree }
+    public enum Topping { Pepperoni, Mushrooms, Onions, Sausage, Bacon, ExtraCheese, Olives, GreenPepper }
+    public enum DrinkSize { Small, Medium, Large }
+    public enum SideType { Fries, OnionRings, GarlicBread, Salad }
+    public enum OrderStatus { Pending, Preparing, Ready, Delivered }
+}
